4739425f134c68e4331a37d8613dc519f253cbc0
Wood Couture <InnovationAI@woodcouture.com>
2025-03-21 15:11:40 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from readability import Document    # type: ignore
+from readability import Document 
 import re
 from urllib.parse import urljoin
 import openai
